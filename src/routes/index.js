fb65b02dc8cf0fbdcffe5d58723664361d8e0682
Leandro Boxler <leandroboxler@gmail.com>
2024-06-19 17:37:10 -0300

    empezando carrito

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 const home = require('../controllers/home');
 const products = require('../controllers/products');
+const cart = require('../controllers/cart');
+
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', home.home);
 router.get('/products', products.product);
+router.get('/cart', cart.cart);
 
 module.exports = router;
